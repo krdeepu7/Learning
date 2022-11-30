7847adc9d92245680a41ba7f7db6ae4ffa473740
DEEPAK  KUMAR <krdeepu777@gmail.com>
2022-11-30 16:06:22 +0530

    Create addition.cpp

---
+++ C++/addition.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+float avg;
+clrscr( );
+cout<<"Enter two Numbers"<<endl;
+cin>>a>>b;
+avg=float(a+b)/2;
+cout<<"Average of two numbers:"<< avg <<endl;
+getch();
+return 0;
+}
